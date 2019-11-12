abb9ae31d960b3e6006a98cfd62451f5b6e6d6c7
rjRenolo <rjrenolo14@gmail.com>
2019-11-12 18:33:35 +0800

    fix

--- src/store.js
+++ src/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
-import thunk from 'redux-thunk';
+import reduxThunk from 'redux-thunk';
 
 const initialState = {};
-const middleware = [thunk];
+// const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
+    applyMiddleware(reduxThunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
