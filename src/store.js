cf4e8bbd309bd03c70d72787092e1bdeb372f8f1
RJ Renolo <52968435+rjRenolo@users.noreply.github.com>
2020-03-21 23:58:28 +0800

    Update store.js

--- src/store.js
+++ src/store.js
@@ -4,13 +4,15 @@ import reduxThunk from 'redux-thunk';
 
 const initialState = {};
 // const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
+/*
+compose(
     applyMiddleware(reduxThunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
+*/
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(reduxThunk)
 );
 export default store;
